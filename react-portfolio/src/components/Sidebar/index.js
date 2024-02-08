14c0e27e162198865e4a98345173cc0d0cb6dc9b
Ryan Chung <ryanchung1430@gmail.com>
2024-02-08 15:32:10 -0500

    initial set up --> left sidebar

---
+++ react-portfolio/src/components/Sidebar/index.js
@@ -0,0 +1,17 @@
+import './index.scss'
+import { Link } from 'react-router-dom'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo-sub.png'
+
+
+const Sidebar = () => (
+    <div className="nav-bar">
+        <Link className="logo" to="/">
+            <img src={LogoS} alt="logo" />
+            <img src={LogoSubtitle} alt="ryan" />
+        </Link>
+    </div>
+
+)
+
+export default Sidebar
